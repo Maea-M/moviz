00b526cc94840f6505f640aab2abe7b35ef590bc
Morgane Mahé <maeamahe@gmail.com>
2023-10-26 19:09:20 +0200

    add pageController

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,11 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_page')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $websiteName = 'Moviz';
         return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+            'websiteName' => $websiteName,
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', [
         ]);
     }
 }
