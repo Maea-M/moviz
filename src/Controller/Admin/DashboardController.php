d8f3f1eb046d33f57ca16b9b444d0ca4767d239d
Morgane Mahé <maeamahe@gmail.com>
2024-03-18 19:13:00 +0100

    make crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,6 +32,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        
+        yield MenuItem::linkToCrud('Genres', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Réalisateurs', 'fas fa-clapperboard', Director::class);
+        yield MenuItem::linkToCrud('Films', 'fas fa-film', Movie::class);
+        yield MenuItem::linkToCrud('Critiques', 'fas fa-star', Review::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
+
     }
 }
