f9db5bdeadaef7221579f77c57d999de4fc112a5
Morgane Mahé <maeamahe@gmail.com>
2024-03-20 16:59:54 +0100

    add vich upload entity movie modified

--- src/Controller/Admin/MovieCrudController.php
+++ src/Controller/Admin/MovieCrudController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Admin;
 
-
 use App\Entity\Movie;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -13,6 +12,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+
 class MovieCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -22,11 +22,17 @@ class MovieCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+        $moviesImagePath =  $mappingsParams['movies']['uri_prefix'];
+
         yield TextField::new('name', 'Nom');
         yield DateField::new('release_date', 'Date de sortie');
+        yield TimeField::new('duration', 'Durée');
+        yield TextEditorField::new('synopsis', 'Synopsis');
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex();
+        yield ImageField::new('imageName', 'Affiche du film')->setBasePath($moviesImagePath)->hideOnForm();
         yield AssociationField::new('directors', 'Réalisateurs');
         yield AssociationField::new('genres', 'Genre');
+
     }
-    
-    
 }
